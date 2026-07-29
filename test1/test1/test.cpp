7961d80ebb5b62c80f7b3b39a1e474ce600eb036
Flipped522 <s9708240905@163.com>
2026-07-29 14:17:27 +0800

    test code

--- test1/test1/test.cpp
+++ test1/test1/test.cpp
@@ -1,25 +1,59 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
 using namespace std;
-void f(int x)
+//void f(int x)
+//{
+//	cout << "f(int x)" << endl;
+//}
+//void f(int* ptr)
+//{
+//	cout << "f(int* ptr)" << endl;
+//}
+//
+//int main()
+//
+//{
+//	f(0);
+//	// 本想通过f(NULL)调⽤指针版本的f(int*)函数，但是由于NULL被定义成0，调⽤了f(int x)，因此与程序的初衷相悖。
+//	f(NULL);
+//	f((int*)NULL);
+//	// f((void*)NULL);
+//
+//	f(nullptr);
+//
+//	return 0;
+//}
+#include<iostream>
+using namespace std;
+
+class A
 {
-	cout << "f(int x)" << endl;
-}
-void f(int* ptr)
-{
-	cout << "f(int* ptr)" << endl;
-}
+private:
+    static int _k;
+    int _h = 1;
+public:
+    class B // B默认就是A的友元
+    {
+    public:
+        void foo(const A& a)
+        {
+            cout << _k << endl;      //OK
+            cout << a._h << endl;    //OK
+        }
+
+        int _b1;
+    };
+};
+
+int A::_k = 1;
 
 int main()
-
 {
-	f(0);
-	// 本想通过f(NULL)调⽤指针版本的f(int*)函数，但是由于NULL被定义成0，调⽤了f(int x)，因此与程序的初衷相悖。
-	f(NULL);
-	f((int*)NULL);
-	// f((void*)NULL);
+    cout << sizeof(A) << endl;
 
-	f(nullptr);
+    A::B b;
+    A aa;
+    b.foo(aa);
 
-	return 0;
+    return 0;
 }
